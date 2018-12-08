f7ebd7ce1c6367eee3beefeb91d4e8ebeaa5040e
Gabriel <gabriel.diasresende@gmail.com>
2018-12-08 20:42:51 -0200

    Equalizacao de histograma

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import sys
 from PIL import Image
 from src.Filters.Suavization import *
+from src.HistEqualizer.HistogramEqualization import *
 import numpy as np
 
 # sys.path.insert(0, r'./')
 
+print("Iniciando Script")
+
 new_filter = SuavizationFilter()
 
+
 imagem = Image.open("IMG1.png").convert('L')
+imagem2 = Image.open("IMG2.png").convert('L')
 width, height = imagem.size
+width2, height2 = imagem2.size
+
+colorDepth = 8
 
 img = np.array(imagem.getdata()).reshape((height, width, 1))
+img2 = np.array(imagem2.getdata()).reshape((height2, width2, 1))
+
+
 
 # Filtragem com filtro media mascara 3
 print('Filtragem com filtro media mascara 3')
@@ -39,3 +50,23 @@ gauss_out = new_filter.gaussian(img, mask=5).evaluate()
 newImage = Image.fromarray(gauss_out.astype('uint8').reshape((height, width)), 'L')
 newImageName = 'IMG1_gauss_5_out.png'
 newImage.save(newImageName)
+
+#Equalização de Histograma para IMG1
+print("Equalização de Histograma para IMG1")
+imgName = "IMG1"
+hist_eq1 = HistogramEqualization(img,colorDepth,imgName)
+img_out = hist_eq1.evaluate()
+newImage = Image.fromarray(img_out.astype('uint8').reshape((height, width)), 'L')
+newImageName = 'IMG1_hist_eq_out.png'
+newImage.save(newImageName)
+
+#Equalização de Histograma para IMG2
+print("Equalização de Histograma para IMG2")
+imgName = "IMG2"
+hist_eq2 = HistogramEqualization(img2,colorDepth,imgName)
+img_out = hist_eq2.evaluate()
+newImage = Image.fromarray(img_out.astype('uint8').reshape((height2, width2)), 'L')
+newImageName = 'IMG2_hist_eq_out.png'
+newImage.save(newImageName)
+
+print("Fim")
---
+++ src/HistEqualizer/HistogramEqualization.py
@@ -0,0 +1,78 @@
+"""
+Histogram Equalization Class
+"""
+
+import numpy
+import math
+import copy
+import matplotlib.pyplot as plt
+
+class HistogramEqualization:
+    """Implements Histogram Equalization"""
+
+    imgName = "IMG" #ImageName
+    colorDepth = 8 #Intensity represented by 8 bits
+
+    def __init__(self, pixels, colorDepth=8, imgName="IMG"):
+        self.pixels = pixels
+        self.colorDepth = colorDepth
+        self.imgName = imgName
+
+    def evaluate(self):
+
+        # Assert pixels is a matrix
+        assert(type(self.pixels) == numpy.ndarray)
+        
+        height, width, _ = self.pixels.shape
+        img = self.pixels.reshape(height*width)
+        
+        L = 2**self.colorDepth
+        
+        # Assert color depth is coherent
+        assert(L > numpy.amax(img))
+
+        # Calculation of intesity frequencies 
+        frequency = numpy.zeros(L)
+        for pixel in img:
+            frequency[pixel] += 1/(width*height)
+        
+        # Print histogram of original image
+        fig_name = self.imgName + "_hist"
+        self.printHistogram(frequency,fig_name)
+        
+        # Creation of intensity transformation function
+        eq_transformation_func = numpy.zeros(L)
+        for intesity in range(L):
+            sum_previous = 0
+            for previous in range(intesity):
+                sum_previous =+ eq_transformation_func[previous]
+            eq_transformation_func[intesity] = sum_previous + (L-1) * frequency[intesity] 
+
+        eq_transformation_func = numpy.around(eq_transformation_func) # Round new intensity values 
+        eq_transformation_func = eq_transformation_func.astype(int) # Transform to integer
+        
+        # Generation of equalized image from the transformation function
+        eq_img = eq_transformation_func[img]
+
+        # Calculation of equalized intesity frequencies 
+        frequency_eq = numpy.zeros(L)
+        for pixel in eq_img:
+            frequency_eq[pixel] += 1/(width*height)
+        
+        # Print histogram of equalized image
+        fig_name = self.imgName + "_eq_hist"
+        self.printHistogram(frequency_eq,fig_name)
+        
+        result = numpy.array(eq_img).reshape((height, width, 1))
+        
+        return result
+
+
+    @staticmethod
+    def printHistogram(frequency, figName):
+        f = plt.figure()    
+        plt.bar(range(len(frequency)),frequency)
+        plt.xlabel("Intensity")
+        plt.ylabel("Frequency")
+        figName = figName + ".pdf" 
+        f.savefig(figName, bbox_inches='tight')
